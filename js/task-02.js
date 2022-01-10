fe0b458df03e362f7cfb8eee09284946b194d986
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-01-10 16:48:56 +0200

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listWithId = document.querySelector('#ingredients');
+ingredients.forEach(ingredient => {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  liEl.classList.add('item');
+  listWithId.append(liEl);
+});
+
+
