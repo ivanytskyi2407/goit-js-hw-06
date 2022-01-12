505f98ec87a84453c7fa3247e7ed59ded82a1870
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-01-12 23:27:25 +0200

    6

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,8 @@
 const textInput = document.querySelector("#validation-input");
 textInput.addEventListener("blur", onInputChange);
 
-function onInputChange(event) {
-  const textInputLength = event.currentTarget.value.length;
+function onInputChange({ currentTarget }) {
+  const textInputLength = currentTarget.value.length;
   const textInputInstallLength = textInput.dataset.length;
   textInput.classList.toggle(
     `valid`,
