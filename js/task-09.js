68e87413d332a3c9d007a473690a44742ff851e8
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-01-12 23:56:51 +0200

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const bodyPage = document.querySelector('body');
+const bodyRandomColor = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+
+btnChangeColor.addEventListener(`click`, () => { 
+ bodyRandomColor.textContent = getRandomHexColor(); 
+bodyPage.style.backgroundColor = getRandomHexColor();
+})
+
+
