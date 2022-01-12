b4edaf9233cfbe3d206176456f95a349003895d0
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-01-13 00:15:03 +0200

    4

--- js/task-04.js
+++ js/task-04.js
@@ -3,10 +3,6 @@ counter: document.querySelector('#value'),
 decrementBtn: document.querySelector('[data-action="decrement"]'),
 incrementBtn: document.querySelector('[data-action="increment"]'),
 };
-
-// const counter = document.querySelector('#value');
-// const decrementBtn = document.querySelector('[data-action="decrement"]');
-// const incrementBtn = document.querySelector('[data-action="increment"]');
 let counterValue = 0;
 refs.decrementBtn.addEventListener("click", () => {
     counterValue -= 1;
