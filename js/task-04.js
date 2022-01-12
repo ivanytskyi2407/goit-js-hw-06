b25b6541f2698a0e67ce44244e436e56e29e1ec1
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-01-13 00:14:53 +0200

    4

--- js/task-04.js
+++ js/task-04.js
@@ -1,14 +1,20 @@
-const counter = document.querySelector('#value');
-const decrementBtn = document.querySelector('[data-action="decrement"]');
-const incrementBtn = document.querySelector('[data-action="increment"]');
+const refs = {
+counter: document.querySelector('#value'),
+decrementBtn: document.querySelector('[data-action="decrement"]'),
+incrementBtn: document.querySelector('[data-action="increment"]'),
+};
+
+// const counter = document.querySelector('#value');
+// const decrementBtn = document.querySelector('[data-action="decrement"]');
+// const incrementBtn = document.querySelector('[data-action="increment"]');
 let counterValue = 0;
-decrementBtn.addEventListener("click", () => {
+refs.decrementBtn.addEventListener("click", () => {
     counterValue -= 1;
-    counter.textContent = counterValue;
+    refs.counter.textContent = counterValue;
 })
-incrementBtn.addEventListener("click", () => { 
+refs.incrementBtn.addEventListener("click", () => { 
     counterValue += 1;
-    counter.textContent = counterValue;
+    refs.counter.textContent = counterValue;
 })
 
 
