06964a9edf4e1c8a874fd736f6226574e1c683a2
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-01-13 13:47:35 +0200

    6

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,10 @@ textInput.addEventListener("blur", onInputChange);
 
 function onInputChange({ currentTarget }) {
   const textInputLength = currentTarget.value.length;
-  const textInputInstallLength = textInput.dataset.length;
+  const textInputInstallLength = Number(textInput.dataset.length);
   textInput.classList.toggle(
     `valid`,
-    textInputLength == textInputInstallLength
+    textInputLength === textInputInstallLength
   );
   textInput.classList.toggle(
     `invalid`,
