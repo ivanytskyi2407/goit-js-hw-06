9fcce51017613e68143232286d53f9918c1dfc66
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-01-15 17:56:09 +0200

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,45 @@
+// Зміні
+const refs = {
+divBoxes: document.querySelector('#boxes'),
+inputRef: document.querySelector('input'),
+createBtn: document.querySelector('[data-create]'),
+destroyBtn: document.querySelector('[data-destroy]'),
+};
+// Випадковий колір
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+refs.inputRef.addEventListener('blur', getAmout)
+
+function getAmout(event) {
+  createBoxes(Number(event.currentTarget.value))
+}
+// Створити
+refs.createBtn.addEventListener('click', createBoxes);
+
+function createBoxes(amout) {
+  let widthHeight = 30;
+  for (let i = 0; i < amout; i++) { 
+    widthHeight += 10;
+const newDiv = document.createElement('div');
+newDiv.style.width = `${widthHeight}px`;
+newDiv.style.height = `${widthHeight}px`;
+newDiv.style.background = getRandomHexColor();
+newDiv.style.margin = '5px'
+refs.divBoxes.append(newDiv);
+  }
+}
+// Очистити
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  refs.divBoxes.innerHTML = '';
+}
+
+
+
+
+
+
+
